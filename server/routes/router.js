7de8f2689410e40123a49a6167c5e32e715e6893
Martincho <martinembon@hotmail.com>
2021-09-09 08:03:10 -0300

    login page

--- server/routes/router.js
+++ server/routes/router.js
@@ -3,18 +3,20 @@ const route = express.Router()
 const services = require("../services/render")
 const controller = require("../controller/controller")
 
-route.get("/",services.homeRoutes)
+route.get("/",services.index)
 
-route.get("/add_user",services.add_user)
+route.get("/dashboard",services.homeRoutes)
 
-route.get("/update_user",services.update_user)
+route.get("/dashboard/add_user",services.add_user)
+
+route.get("/dashboard/update_user",services.update_user)
 
 //API
 
-route.post("/api/users",controller.create)
-route.get("/api/users",controller.find)
+route.post("/dashboard/api/users",controller.create)
+route.get("/dashboard/api/users",controller.find)
 //route.get("/api/users/:id",controller.findOne)
-route.put('/api/users/:id', controller.update);
-route.delete("/api/users/:id",controller.delete)
+route.put('/dashboard/api/users/:id', controller.update);
+route.delete("/dashboard/api/users/:id",controller.delete)
 
 module.exports = route
--- server/services/render.js
+++ server/services/render.js
@@ -1,12 +1,17 @@
 const axios = require("axios")
+
+exports.index=(req,res)=>{
+    res.render("index")
+}
+
 //axios.get("https://localhost:3000/api/users")
 //axios.get("https://crud-delivery.herokuapp.com/api/users")
 exports.homeRoutes=(req,res)=>{
     //mage a get req to the /api/users
-    axios.get("https://crud-delivery.herokuapp.com/api/users")    
+    axios.get("https://crud-delivery.herokuapp.com/dashboard/api/users")    
         .then(response=>{
             //console.log(response.data)            
-            res.render("index",{users:response.data})        
+            res.render("dashboard",{users:response.data})        
         })
         .catch(err=>{
             res.send(err)
@@ -18,8 +23,9 @@ exports.add_user=(req,res)=>{
     res.render("add_user")
 }
 
+
 exports.update_user = (req, res) =>{
-    axios.get('https://crud-delivery.herokuapp.com/api/users', { params : { id : req.query.id }})
+    axios.get('https://crud-delivery.herokuapp.com/dashboard/api/users', { params : { id : req.query.id }})
         .then(function(userdata){
             res.render("update_user", { user : userdata.data})            
         })
